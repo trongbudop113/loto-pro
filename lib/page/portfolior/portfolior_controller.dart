b0d46b5f0ba97df61a3171b1711ec04ddada22ca
luuhoangtrong <trongbudop113@gmail.com>
2023-12-17 13:18:05 +0700

    + Commit body

--- lib/page/portfolior/portfolior_controller.dart
+++ lib/page/portfolior/portfolior_controller.dart
@@ -13,7 +13,7 @@ class PortFoliorController extends GetxController {
   String linkin =
       "https://www.linkedin.com/in/l%C6%B0u-ho%C3%A0ng-tr%E1%BB%8Dng-b87970140?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
 
-  PageController controller = PageController();
+  PageController pageController = PageController();
   final List<Widget> listPage = <Widget>[
     const Center(
       child: Pages(
@@ -37,4 +37,14 @@ class PortFoliorController extends GetxController {
     )
   ];
   final RxInt curr = 0.obs;
+
+  void onTapForward() {
+    if(curr.value == 0) return;
+    pageController.animateToPage(curr.value - 1, duration: Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
+
+  void onTapNext() {
+    if(curr.value == listPage.length - 1) return;
+    pageController.animateToPage(curr.value + 1, duration: Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
 }
--- lib/page/portfolior/portfolior_page.dart
+++ lib/page/portfolior/portfolior_page.dart
@@ -30,38 +30,87 @@ class PortFoliorPage extends GetView<PortFoliorController> {
     // );
     return Scaffold(
       backgroundColor: Colors.grey,
-      appBar: AppBar(
-        title: Text("portfolior".tr),
-        backgroundColor: Colors.green,
-      ),
       body: Stack(
         children: [
           PageView(
             scrollDirection: Axis.horizontal,
-            controller: controller.controller,
+            controller: controller.pageController,
             onPageChanged: (num) {
               controller.curr.value = num;
             },
             children: controller.listPage,
           ),
           Positioned(
+            top: MediaQuery.of(context).padding.top + 25,
+            left: 25,
+            child: GestureDetector(
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.orangeAccent
+                ),
+              ),
+              onTap: (){
+                Get.back();
+              },
+            ),
+          ),
+          Positioned(
             bottom: 50,
-            left: 50,
-            right: 50,
+            left: 20,
+            right: 20,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: List.generate(controller.listPage.length, (e){
-                return Container(
-                  width: 40,
-                  height: 40,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(360),
-                    color: Colors.amber
+              children: [
+                GestureDetector(
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.orangeAccent
+                    ),
                   ),
-                  child: Text("${e + 1}"),
-                );
-              }),
+                  onTap: (){
+                    controller.onTapForward();
+                  },
+                ),
+                SizedBox(width: 20,),
+                Expanded(
+                  flex: 10,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: List.generate(controller.listPage.length, (e){
+                      return Container(
+                        width: 40,
+                        height: 40,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(360),
+                          color: Colors.amber
+                        ),
+                        child: Text("${e + 1}"),
+                      );
+                    }),
+                  ),
+                ),
+                SizedBox(width: 20,),
+                GestureDetector(
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.orangeAccent
+                    ),
+                  ),
+                  onTap: (){
+                    controller.onTapNext();
+                  },
+                ),
+              ],
             ),
           )
         ],
